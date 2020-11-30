66cec6161b354fbee3ae8092345854fb98301800
pfreemanc <pfreemanc@gmail.com>
2020-11-30 12:41:39 -0500

    starting to get set up, files deleted as per the udemy instructions (first time deleting and working with index.js, we shall see)

---
+++ seasons/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      lat: null,
+    }
+  }
+
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return(
+      <div>Latitude:</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
